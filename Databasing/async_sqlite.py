d185007be697c7decffd1e39f4a0e46c67161554
Pitta Andrew Blaze <andrewblaze2719@gmail.com>
2023-12-30 15:30:56 +0530

    simple sql updates not done for ajay doing

--- Databasing/async_sqlite.py
+++ Databasing/async_sqlite.py
@@ -3,26 +3,37 @@ import asyncio
 class SqliteDB:
     def __init__(self, db_path):
         self.db_path = db_path
+        self.connection = None
+    #     store the connection to a varaible and use it in all the methods
+    #     self.connection = await aiosqlite.connect(self.db_path)
+
+    async def create_table(self,query):
+        await self.connection.execute(query)
+        await self.connection.commit()
+
 
     async def setup(self):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute('''CREATE TABLE IF NOT EXISTS communications 
-                                (server_id TEXT, data_sent TEXT, response_received TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
-            await db.commit()
+        self.connection = await aiosqlite.connect(self.db_path)
+        # create server table
+        await self.create_table("CREATE TABLE IF NOT EXISTS servers (server_ip TEXT, hash TEXT)")
+        # file table
 
-    async def insert_communication(self, server_id, data_sent, response_received):
-        async with aiosqlite.connect(self.db_path) as db:
-            await db.execute("INSERT INTO communications (server_id, data_sent, response_received) VALUES (?, ?, ?)",
-                             (server_id, data_sent, response_received))
-            await db.commit()
 
-    async def fetch_all_communications(self):
-        async with aiosqlite.connect(self.db_path) as db:
-            cursor = await db.execute("SELECT * FROM communications")
-            rows = await cursor.fetchall()
-            return rows
 
-# obj = SqliteDB('test.db')
-# asyncio.run(obj.setup())
-# asyncio.run(obj.insert_communication('server1', 'Hello, Server!', 'Data received'))
-# asyncio.run(obj.insert_communication('server2', 'Hello, Server!', 'Data received'))
+
+class Clinet_operations(SqliteDB):
+    def __init__(self, db_path):
+        super().__init__(db_path)
+        self.client_connection = None
+
+    async def connect(self):
+        await super().setup()
+        self.client_connection = await aiosqlite.connect(self.db_path)
+    async def insert_server(self,server_ip,hash):
+        pass
+
+    async def file_data(self,**kwargs):
+        file_name = kwargs.get("file_name")
+        extension = kwargs.get("extension")
+        hash = kwargs.get("hash")
+        size = kwargs.get("size")
